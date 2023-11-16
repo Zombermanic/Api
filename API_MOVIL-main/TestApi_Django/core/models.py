a6509374252bf2a76e2541b924dccde543a5e64c
Zombermanic <leocacru@gmail.com>
2023-11-16 19:06:30 -0300

    arreglo api
    funcinando

--- API_MOVIL-main/TestApi_Django/core/models.py
+++ API_MOVIL-main/TestApi_Django/core/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Alumno(models.Model):
     idAlumno = models.IntegerField(primary_key=True, verbose_name = "Id de Usuario")
     Gmail = models.CharField(max_length=50, verbose_name = "Gmail")
-    Contrasena = models.CharField(max_length=50, verbose_name = "Contrasena")
-    nombreAlumno = models.CharField(max_length=50, verbose_name = "Nombre de Usuario")
+    password = models.CharField(max_length=50, verbose_name = "Contrasena")
+    user = models.CharField(max_length=50, verbose_name = "Nombre de Usuario")
 
     def __str__(self):
-        return self.nombreAlumno
+        return self.user
