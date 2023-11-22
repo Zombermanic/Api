f1d2f0722f88c777ee6018641a35937b3bce225c
Zombermanic <leocacru@gmail.com>
2023-11-21 22:57:49 -0300

    alguans cosas que podrian cambiar

--- API_MOVIL-main/TestApi_Django/core/models.py
+++ API_MOVIL-main/TestApi_Django/core/models.py
@@ -11,10 +11,10 @@ class Alumno(models.Model):
         return self.user
 
 class Conductor(models.Model):
-    idConductor = models.IntegerField(primary_key=True, verbose_name = "Id de Usuario")
+    idConductor = models.IntegerField(primary_key=True, verbose_name = "Id de Conductor")
     Gmail = models.CharField(max_length=50, verbose_name = "Gmail")
-    password = models.CharField(max_length=50, verbose_name = "Contrasena")
-    user = models.CharField(max_length=50, verbose_name = "Nombre de Usuario")
+    password = models.CharField(max_length=50, verbose_name = "Contrasena")    
+    user = models.CharField(max_length=50, verbose_name = "Nombre de Conductor")
 
     def __str__(self):
         return self.user
